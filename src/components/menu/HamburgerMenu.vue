bdda8fd8c8f04f03735ac5c72d0acc906d12c5d2
Sam Garg <s.garg@astoundcommerce.com>
2022-11-18 15:46:02 +0000

    CSS Specificity

--- src/components/menu/HamburgerMenu.vue
+++ src/components/menu/HamburgerMenu.vue
@@ -5,34 +5,40 @@ import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
 </script>
 
 <template>
-  <Push :closeOnNavigation="true" left>
-    <Nav :showLogo="false"/>
-    <LanguageSwitcher class="hero__language-switcher"/>
-  </Push>
+  <div class="hamburger-menu">
+    <Push :closeOnNavigation="true" left>
+      <Nav :showLogo="false"/>
+      <LanguageSwitcher class="hero__language-switcher"/>
+    </Push>
+  </div>
 </template>
 
 <style lang="scss">
-.bm-burger-button {
-  z-index: 999;
-  position: fixed;
-}
-.bm-burger-bars {
-  background-color: white;
-}
 
-.bm-menu {
-  background-color: var(--c-black);
-  line-height: 2;
-  a {
-    color: var(--c-white);
+.hamburger-menu {
+  .bm-burger-button {
+    z-index: 999;
+    position: fixed;
   }
 
-  .bm-item-list>* {
-    display: block;
+  .bm-burger-bars {
+    background-color: white;
   }
 
-  .site-logo {
-    display: none;
+  .bm-menu {
+    background-color: var(--c-black);
+    line-height: 2;
+    a {
+      color: var(--c-white);
+    }
+
+    .bm-item-list>* {
+      display: block;
+    }
+
+    .site-logo {
+      display: none;
+    }
   }
 }
 </style>
