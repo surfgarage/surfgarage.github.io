da80479bac74bbd7c38329b53f0e2228bdf449ad
Sam Garg <s.garg@astoundcommerce.com>
2022-12-04 17:23:09 +0000

    deploy

--- assets/arrow-down.da41ce1d.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,a as t}from"./index.a1b5ae29.js";const s={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 64 64"},n=t("path",{d:"M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30 30-13.432 30-30S48.568 2 32 2zm0 47L16 33.695h10.857V15h10.285v18.695H48L32 49z",fill:"#fff"},null,-1),r=[n];function c(a,l){return e(),o("svg",s,r)}const h={render:c};export{h as default,c as render};
