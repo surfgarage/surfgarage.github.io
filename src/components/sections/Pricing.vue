7d43840b50e44d9099daefb3cd9b7829100a9fea
Sam Garg <s.garg@astoundcommerce.com>
2023-05-27 07:21:04 +0100

    pricing updated

--- src/components/sections/Pricing.vue
+++ src/components/sections/Pricing.vue
@@ -1,14 +1,33 @@
 <template>
   <section id="pricing" class="section pricing">
-    <h2 class="pricing__title section__title">{{ $t("pricing.title") }}</h2>
+    <h2 class="pricing__section-title section__title">
+      {{ $t("pricing.title") }}
+    </h2>
     <ul class="section__content pricing__content">
       <li class="pricing__content-item">
-        <h3 class="pricing__month">{{ $t("pricing.monthly.title") }}</h3>
-        <h4 class="pricing__price">{{ $t("pricing.monthly.price") }}</h4>
+        <h3 class="pricing__title">{{ $t("pricing.monthly.title") }}</h3>
+        <h4 class="pricing__perMonth">{{ $t("pricing.monthly.price") }}</h4>
       </li>
       <li class="pricing__content-item">
-        <h3 class="pricing__month">{{ $t("pricing.3months.title") }}</h3>
-        <h4 class="pricing__price">{{ $t("pricing.3months.price") }}</h4>
+        <h3 class="pricing__title">{{ $t("pricing.3months.title") }}</h3>
+        <h4 class="pricing__perMonth">
+          {{ $t("pricing.3months.perMonth") }}
+        </h4>
+        <div class="pricing__price">({{ $t("pricing.3months.price") }})</div>
+      </li>
+      <li class="pricing__content-item">
+        <h3 class="pricing__title">{{ $t("pricing.6months.title") }}</h3>
+        <h4 class="pricing__perMonth">
+          {{ $t("pricing.6months.perMonth") }}
+        </h4>
+        <div class="pricing__price">({{ $t("pricing.6months.price") }})</div>
+      </li>
+      <li class="pricing__content-item">
+        <h3 class="pricing__title">{{ $t("pricing.1year.title") }}</h3>
+        <h4 class="pricing__perMonth">
+          {{ $t("pricing.1year.perMonth") }}
+        </h4>
+        <div class="pricing__price">({{ $t("pricing.1year.price") }})</div>
       </li>
     </ul>
   </section>
@@ -19,7 +38,7 @@
   background-color: var(--c-wave-blue);
   text-align: center;
 
-  &__title {
+  &__section-title {
     padding-top: 5rem;
     color: #fff;
   }
@@ -28,15 +47,15 @@
     padding-top: 5rem;
     @include respond-to("tablet-large") {
       display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-gap: 10rem;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      grid-gap: 4rem;
       padding-top: 10rem;
       padding-bottom: 10rem;
     }
   }
 
   &__content-item {
-    padding: 10rem;
+    padding: 2rem;
     text-align: center;
     background-color: #fff;
     margin-bottom: 2rem;
@@ -47,13 +66,21 @@
 
     @include respond-to("tablet-large") {
       outline: 2px solid #fff;
-      outline-offset: 2rem;
+      outline-offset: 1rem;
       margin-bottom: 0;
+      padding: 5rem;
     }
   }
 
-  &__month {
-    margin-bottom: 2rem;
+  &__title {
+    margin-bottom: 1rem;
+  }
+  &__perMonth {
+    font-size: 1.8rem;
+  }
+  &__price {
+    margin-top: 0.6rem;
+    font-size: 1.8rem;
   }
 }
 </style>
